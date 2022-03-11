ea16f9105e38735bc2f1eeb73b26768fe799304b
HaveToDo <zmfpdkwl2@gmail.com>
2022-03-11 17:26:54 +0900

    [0311] 쇼핑 상품 목록 페이지와 상세 페이지 개발

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,94 @@
 <template>
-  <div>
-    <p>넉스트 시작하기</p>
+  <div class="app">
+    <main>
+      <div>
+        <input type="text" />
+      </div>
+      <ul>
+        <li
+          v-for="product in products"
+          :key="product.id"
+          class="item flex"
+          @click="moveToDetailPage(product.id)"
+        >
+          <img
+            class="product-image"
+            :src="product.imageUrl"
+            :alt="product.name"
+          />
+          <p>{{ product.name }}</p>
+          <span>{{ product.price }}</span>
+        </li>
+      </ul>
+    </main>
   </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+
+export default {
+  async asyncData() {
+    const response = await axios.get('http://localhost:3000/products')
+    const products = response.data.map((item) => {
+      return {
+        ...item,
+        imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+      }
+    })
+    return { products }
+  },
+  methods: {
+    moveToDetailPage(id) {
+      console.log(id)
+      this.$router.push(`detail/${id}`)
+    },
+  },
+  // data() {
+  //   return {
+  //     products: [],
+  //   }
+  // },
+
+  // spa의 데이터 호출 방법
+  // async created() {
+  //   const response = await axios.get('http://localhost:3000/products')
+  //   console.log(response)
+  //   this.products = response.data
+  // },
+}
 </script>
 
-<style></style>
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+}
+.item {
+  display: inline-block;
+  width: 400px;
+  height: 300px;
+  text-align: center;
+  margin: 0 0.5rem;
+  cursor: pointer;
+}
+.product-image {
+  width: 400px;
+  height: 250px;
+}
+.app {
+  position: relative;
+}
+.cart-wrapper {
+  position: sticky;
+  float: right;
+  bottom: 50px;
+  right: 50px;
+}
+.cart-wrapper .btn {
+  display: inline-block;
+  height: 40px;
+  font-size: 1rem;
+  font-weight: 500;
+}
+</style>
