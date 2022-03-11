3e11fd4d6d6795a35c1c7d30b2de613cf0346203
정종찬 <jeongjongchan@jeongjongchan-ui-MacBookAir.local>
2022-03-11 15:26:57 +0900

    [0311] Nuxt의 데이터 호출 방식과 API 연동

---
+++ components/ProductList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ products }}
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+
+export default {
+  // asyncData는 pages 아래에 있는 컴포넌트에서만 사용 가능하다.
+  // async asyncData() {
+  //   console.log(1)
+  //   const response = await axios.get('http://localhost:3000/products')
+  //   console.log(response)
+  //   const products = response.data
+  //   return { products }
+  // },
+}
+</script>
+
+<style></style>
--- pages/mainPage.vue
+++ pages/mainPage.vue
@@ -1,11 +1,36 @@
 <template>
   <div>
     <p>메인 페이지입니다</p>
+    <product-list />
   </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+import ProductList from '../components/ProductList.vue'
+
+export default {
+  components: { ProductList },
+  async asyncData() {
+    const response = await axios.get('http://localhost:3000/products')
+    console.log(response)
+    const products = response.data
+    return { products }
+  },
+
+  // data() {
+  //   return {
+  //     products: [],
+  //   }
+  // },
+
+  // spa의 데이터 호출 방법
+  // async created() {
+  //   const response = await axios.get('http://localhost:3000/products')
+  //   console.log(response)
+  //   this.products = response.data
+  // },
+}
 </script>
 
 <style></style>
